fc0ab58bb76d0994d576be1f46fb17e5082d3ed4
omletccblpom <tima.ledyaev@mail.ru>
2024-06-13 14:54:40 +0300

    conf py del

--- exam/config.py
+++
@@ -1,5 +0,0 @@
-SECRET_KEY = '3c34c03d6d9d88e3f7cfbb8c16c5b602'
-MYSQL_USER = 'std_2630_exam'
-MYSQL_PASSWORD = 'Password'
-MYSQL_HOST = 'std-mysql.ist.mospolytech.ru'
-MYSQL_DATABASE = 'std_2630_exam'
